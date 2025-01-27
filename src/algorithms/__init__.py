c17da5a54527b9dc1083e55853d267fd6f7c3640
CheeseLoverMax <129116708+CheeseLoverMax@users.noreply.github.com>
2025-01-27 13:23:24 +0000

    Revert "Add files via upload"

--- src/algorithms/__init__.py
+++
@@ -1,3 +0,0 @@
-# File: /elevator-simulator/elevator-simulator/src/algorithms/__init__.py
-
-# This file is intentionally left blank.
--- src/config/settings.py
+++
@@ -1,9 +0,0 @@
-# settings.py
-
-DEFAULT_FLOORS = 10
-DEFAULT_ELEVATORS = 1
-
-config = {
-    "floors": DEFAULT_FLOORS,
-    "elevators": DEFAULT_ELEVATORS
-}
