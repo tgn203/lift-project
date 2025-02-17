2858594f9ec712daf60fba4b5b44dfae3cc7bb45
Max Read <129116708+CheeseLoverMax@users.noreply.github.com>
2025-02-17 17:45:40 +0000

    Fixed File Pathing

--- src/src/building.py
+++
@@ -1,7 +0,0 @@
-class Building:
-    def __init__(self, num_floors=10):
-        self.num_floors = num_floors
-        self.elevators = []
-    
-    def add_elevator(self, elevator):
-        self.elevators.append(elevator)
